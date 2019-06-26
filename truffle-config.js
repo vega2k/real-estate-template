72ae09fdc98528d2cdba01b8cc5d29e0739829ce
vega2k <vega2k@naver.com>
2019-06-27 00:10:06 +0900

    real-estate template

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,22 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "drive ....";
+
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
      networks: {
           ganache: {
-               host: "localhost",
+               host: "127.0.0.1",
                port: 8545,
                network_id: "*" // Match any network id
+          },
+          ropsten: {
+               provider: function () {
+                    return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/79bfa7e03d7e4af6b08d066b85cf7876');
+               },
+               network_id: '3',
+               gas: 4500000,
+               gasPrice: 10000000000,
           }
      }
 };
